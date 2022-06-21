aa8fddc8437d684ff7358b454decc37f30399b73
Peretz83 <liavperetz83@gmail.com>
2022-06-21 14:54:31 +0300

    First commit
    First homework

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+var firstP ='I really am afraid of JS';
+console.log(firstP);
+
+var secondP = 'Loving JS';
+console.log(secondP);
+
+var thirdP = firstP.slice(0,12);
+console.log(thirdP);
+
+var forthP = thirdP + secondP;
+console.log(forthP);
